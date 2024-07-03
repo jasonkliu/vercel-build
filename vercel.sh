689441f820dd551a80a103fd869d002829e2cf1c
Jason Liu <jasonkliu@gmail.com>
2024-07-03 13:10:08 -0600

    working script

--- vercel.sh
+++ vercel.sh
@@ -3,17 +3,23 @@
 # If a change is made only to backend/, do not build
 # Otherwise, build (including src/ and root directory)
 # `git diff` doesn't return correct error codes, so need to be creative
-# git diff HEAD^ HEAD --quiet -- ./backend/
 
 set -ex
 
 check_diff() {
+    echo $PWD
+    # This works, but gives "yes" for 1 and 3, which is undesirable
     # git diff HEAD^ HEAD -- ./backend/
     # [ -n "$(git diff HEAD^ HEAD -- ./backend/)" ] && echo "yes"
-    # echo $?
-    echo $PWD
+
+    # Ideally, we return "no" for 1 and "yes" for 2 and 3
     git diff HEAD^ HEAD -- . ":!./backend"
-    echo $?
+    output=$(git diff HEAD^ HEAD -- . ":!./backend")
+    if [ -n "$output" ]; then
+        echo 1
+    else
+        echo 0
+    fi
 }
 
 
@@ -28,4 +34,3 @@ cd ../
 cd 3_both/
 check_diff
 cd ../
-
